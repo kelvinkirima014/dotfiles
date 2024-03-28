1cdc1c68c7d97c555e5fb71ba7f82d8a6794e311
kelvinkirima014 <kelvinkirima2@gmail.com>
2024-03-28 06:49:46 +0300

    change polybar fonts

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar --reload second &
+done
+
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar main --config=~/.config/polybar/config.ini &
+# polybar main --config=~/.config/polybar/config.ini &
 # polybar second --config=~/.config/polybar/config.ini &
 
 echo "Polybar launched..."
